1123a69c4ac2475651a9d36b6e684f157eb4eecd
Mimoh Kulkarni <mimohkulkarni17@gmail.com>
2020-11-26 22:25:54 +0530

    new edits

--- backend/courseDetails.js
+++ backend/courseDetails.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
     // alert("ok");
     let searchParams = new URLSearchParams(window.location.search);
     let subSemId;
+    let units = [];
     if(searchParams.has('id')){
         subSemId = searchParams.get('id');
         // alert(subSemId);
@@ -117,6 +118,7 @@ $(document).ready(function(){
                     
                 }
                 else{
+                    // alert("herre");
                     $('#courseSyllabus').append("<div class='row'><div class='col-sm-12'><h5 class='text-center'>No data to fetch</h5></div</div>");
                 }
             },
@@ -125,9 +127,291 @@ $(document).ready(function(){
             }
         });
 
+    });
 
+    let courseBookmarkedFlag = false;
+    let courseYourContentFlag = false;
 
+    $("#nav-content-tab").click(function(){
+        // alert("The paragraph was clicked.");
+        
+        let courseUnits = $('#courseUnits').val();
 
-      });
+        $.ajax({
+            url: 'https://stagingfacultypython.edwisely.com/getCourseSyllabus?subject_semester_id='+subSemId,
+            type: 'GET',
+            contentType: 'application/json',
+            headers: {
+                'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxMTMwLCJlbWFpbCI6InByYWthc2hAZWR3aXNlbHkuY29tIiwiaW5pIjoiMTYwNjIzMjkxOCIsImV4cCI6IjE2MDc1Mjg5MTgifQ.i1TImgHIZx5cP6L7TAYrEwpBVpbsjmsF1mvqmiEolo4'
+            },
+            success: function (result) {
+                // alert(result.status);
+                // $('#courseSyllabus').empty();
+                if(result.status == 200 && result.data != null){
+                    if(!courseUnits){
+                        let units = [];
+                        $('#courseUnits').empty();
+                        $.each(result.data , function(key , value){
+                            // alert(value);
+                            units.push({"id":value.id,"name":value.name});
+                            $('#courseUnits').append("<option value="+value.id+">"+value.name+"</option>"); 
+                        });
+                        $('#courseUnits').val(units[0]['id']);
+                        courseUnits = units[0]['id'];
+                        // alert(units[0]['id']);
+                    }
+                    
+                    // 2nd api
+
+                    
+                    let courseType = $('#courseType').val();
+                    let courseLevel = $('#courseLevel').val();
+                    // let courseCatagory = $('#courseCatagory').val();
+                    // courseUnits = $('#courseUnits').val();
+
+                    // alert(courseUnits);
+
+                    if(!courseBookmarkedFlag && !courseYourContentFlag){
+
+                        $.ajax({
+                            url: 'https://stagingfacultypython.edwisely.com/getCourseContent?unit_id='+courseUnits+'&subject_semester_id='+subSemId,
+                            type: 'GET',
+                            contentType: 'application/json',
+                            headers: {
+                                'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxMTMwLCJlbWFpbCI6InByYWthc2hAZWR3aXNlbHkuY29tIiwiaW5pIjoiMTYwNjIzMjkxOCIsImV4cCI6IjE2MDc1Mjg5MTgifQ.i1TImgHIZx5cP6L7TAYrEwpBVpbsjmsF1mvqmiEolo4'
+                            },
+                            success: function (result) {
+                                // alert(result.status);
+                                $('#courseFiles').empty();
+                                // let div = "";
+                                let files = [];
+                                if(result.status == 200 && (result.academic_materials || result.learning_content)){
+                                    
+                                    files = processFiles(result);
+
+                                    // alert(files);
+
+                                    displayFiles(files,courseType,courseLevel);
+
+                                }
+                                else{
+                                    // alert("here1");
+                                    $('#courseFiles').append("<div class='row'><div class='col-sm-12'><h5 class='text-center'>No data to fetch</h5></div</div>");
+                                }
+                            },
+                            error: function (error) {
+                                alert(result.message);
+                            }
+                        });
+                    }
+                    else if(courseBookmarkedFlag){
+
+                        courseBookmarkedFlag = false;
+
+                        $.ajax({
+                            url: 'https://stagingfacultypython.edwisely.com/getFacultyBookmarkedCourseContent?unit_id='+courseUnits+'&subject_semester_id='+subSemId,
+                            type: 'GET',
+                            contentType: 'application/json',
+                            headers: {
+                                'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxMTMwLCJlbWFpbCI6InByYWthc2hAZWR3aXNlbHkuY29tIiwiaW5pIjoiMTYwNjIzMjkxOCIsImV4cCI6IjE2MDc1Mjg5MTgifQ.i1TImgHIZx5cP6L7TAYrEwpBVpbsjmsF1mvqmiEolo4'
+                            },
+                            success: function (result) {
+                                // alert(result.learning_content);
+                                $('#courseFiles').empty();
+                                // let div = "";
+                                let files = [];
+                                if(result.status == 200 && (result.academic_materials || result.learning_content)){
+                                    
+                                    files = processFiles(result);
+
+                                    // alert(files);
+
+                                    displayFiles(files,courseType,courseLevel);
+
+                                }
+                                else{
+                                    // alert("here1");
+                                    $('#courseFiles').append("<div class='row'><div class='col-sm-12'><h5 class='text-center'>No data to fetch</h5></div</div>");
+                                }
+                            },
+                            error: function (error) {
+                                alert(result.message);
+                            }
+                        });
+
+                    }
+                    else if(courseYourContentFlag){
+
+                        courseYourContentFlag = false;
+
+                        $.ajax({
+                            url: 'https://stagingfacultypython.edwisely.com/getFacultyAddedCourseContent?unit_id='+courseUnits+'&subject_semester_id='+subSemId,
+                            type: 'GET',
+                            contentType: 'application/json',
+                            headers: {
+                                'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxMTMwLCJlbWFpbCI6InByYWthc2hAZWR3aXNlbHkuY29tIiwiaW5pIjoiMTYwNjIzMjkxOCIsImV4cCI6IjE2MDc1Mjg5MTgifQ.i1TImgHIZx5cP6L7TAYrEwpBVpbsjmsF1mvqmiEolo4'
+                            },
+                            success: function (result) {
+                                // alert(result.academic_materials);
+                                $('#courseFiles').empty();
+                                // let div = "";
+                                let files = [];
+                                if(result.status == 200 && result.data){
+                                    $.each(result.data , function(key , value){
+                                        files.push({
+                                            "material_id":value.material_id,
+                                            "title":value.title,
+                                            "url":value.file_url,
+                                            "type":value.type,
+                                            "level":value.level,
+                                            "bookmarked":value.bookmarked
+                                        });
+                                        // div = div + "</div>";
+                                    });
+
+                                    displayFiles(files,courseType,courseLevel);
+
+                                }
+                                else{
+                                    // alert("here1");
+                                    $('#courseFiles').append("<div class='row'><div class='col-sm-12'><h5 class='text-center'>No data to fetch</h5></div</div>");
+                                }
+                            },
+                            error: function (error) {
+                                alert(result.message);
+                            }
+                        });                        
+
+                    }
+
+                }
+                else{
+                    $('#courseFiles').append("<div class='row'><div class='col-sm-12'><h5 class='text-center'>No data to fetch</h5></div</div>");
+                    alert("here");
+                }
+            },
+            error: function (error) {
+                alert(result.message);
+            }
+        });
+    });
+
+    $('#courseType').on('change', function() {
+        $("#nav-content-tab").click();
+    });
+
+    $('#courseLevel').on('change', function() {
+        $("#nav-content-tab").click();
+    });
+
+    $('#courseUnits').on('change', function() {
+        $("#nav-content-tab").click();
+    });
+
+    $('#courseCatagory').on('change', function() {
+        let courseCatagory = $('#courseCatagory').val();
+        if(courseCatagory == "bookmarked"){
+            courseBookmarkedFlag = true;
+            $("#nav-content-tab").click();
+        }
+        else if(courseCatagory == "yourContent"){
+            courseYourContentFlag = true;
+            $("#nav-content-tab").click();
+        }
+        else
+            $("#nav-content-tab").click();
+    });
+
+    function processFiles(result = []){
+
+        let files = [];
+
+        $.each(result.academic_materials , function(key , value){
+            files.push({
+                "material_id":value.material_id,
+                "title":value.title,
+                "url":value.file_url,
+                "type":value.type,
+                "level":value.level,
+                "bookmarked":value.bookmarked
+            });
+            // div = div + "</div>";
+        });
+        
+        $.each(result.learning_content , function(key , value){
+            // alert(value.title);
+            files.push({
+                "material_id":value.material_id,
+                "title":value.title,
+                "url":value.file_url,
+                "type":value.type,
+                "level":value.level,
+                "bookmarked":value.bookmarked
+            });
+            // alert(value);
+        });
+
+        return files;
+    }
+
+    function displayFiles(files = [],courseType,courseLevel){
+
+        let div = "";
+
+        if(files){
+            $.each(files , function(key , value){
+                if((courseType == value.type || courseType == "0") && (courseLevel == value.level || courseLevel == "0")){
+                    div = div + "<div class='row'>";
+                    div = div + "<div class='col-sm-1 d-flex justify-content-end'>";
+                    switch(value.type){
+                        case "DOCS":
+                            div = div + "<i class='fa fa-file-pdf fa-2x' aria-hidden='true'></i>";
+                            break;
+                        case "PDF":
+                            div = div + "<i class='fa fa-file-pdf fa-2x' aria-hidden='true'></i>";
+                            break;
+                        case "VIDEO":
+                            div = div + "<i class='fa fa-video-pdf fa-2x' aria-hidden='true'></i>";
+                            break;
+                        case "MP4":
+                            div = div + "<i class='fa fa-video-pdf fa-2x' aria-hidden='true'></i>";
+                            break;
+                        case "PPT":
+                            div = div + "<i class='fa fa-file-excel fa-2x' aria-hidden='true'></i>";
+                            break;
+                        case "URL":
+                            div = div + "<i class='fa fa-file-zip fa-2x' aria-hidden='true'></i>";
+                            break;
+                        default:
+                            div = div + "<i class='fa fa-file-pdf fa-2x' aria-hidden='true'></i>";
+                    }
+                    // div = div + "<i class='fa fa-file-pdf fa-2x' aria-hidden='true'></i>";
+                    div = div + "</div>";
+                    div = div + "<div class='col-sm-10 d-flex justify-content-start align-middle download' style='cursor:pointer;' data-url='"+value.url+"'>";
+                    div = div + "<h6>"+value.title+"</h6>";
+                    div = div + "</div>";
+                    div = div + "<div class='col-sm-1'>";
+                    div = div + "<div class='dropdown'>";
+                    div = div + "<button class='btn dropdown-toggle' type='button' id='dropdownMenuButton' data-toggle='dropdown' aria-haspopup='true' aria-expanded='false'>";
+                    div = div + "<i class='fa fa-cog' aria-hidden='true'></i>";
+                    div = div + "</button>";
+                    div = div + "<div class='dropdown-menu' aria-labelledby='dropdownMenuButton'>";
+                    div = div + "<a class='dropdown-item' href='#'>Edit</a>";
+                    div = div + "<a class='dropdown-item' href='#'>Bookmarked</a>";
+                    div = div + "<a class='dropdown-item' href='#'>Delete</a>";
+                    div = div + "</div></div></div></div>";
+                }
+                // alert(value);
+            });
+            if(div)
+                $('#courseFiles').append(div);
+            else
+            $('#courseFiles').append("<div class='row'><div class='col-sm-12'><h5 class='text-center'>No data to fetch</h5></div</div>");
+        }
+        else
+            $('#courseFiles').append("<div class='row'><div class='col-sm-12'><h5 class='text-center'>No data to fetch</h5></div</div>");
+    }
+
+      
 
 });
