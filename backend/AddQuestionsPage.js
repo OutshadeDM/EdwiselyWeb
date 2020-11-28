1d6a613c2c1c8f563f9508cbefdef0d5a4b884ff
Akshat <akshatsaraswat03@gmail.com>
2020-11-28 20:24:53 +0530

    pages for assessments

--- backend/AddQuestionsPage.js
+++ backend/AddQuestionsPage.js
@@ -2,14 +2,17 @@ $(document).ready(function () {
 
   $(document).on('click', '#addques', function (event) {
     alert('add question')
+    event.preventDefault();
+    var url = $(this).data('target');
+    location.replace(url);
   })
 
   $(document).on('click', '#uploadques', function (event) {
-    alert('add question')
+    alert('upload question')
   })
 
   $(document).on('click', '#chooseques', function (event) {
-    alert('add question')
+    alert('choose question')
   })
 
 });
