0e1e67ea77365361b641e74781c80d6db6433bd7
Mimoh Kulkarni <mimohkulkarni17@gmail.com>
2020-12-05 17:57:37 +0530

    create add course

--- backend/addCourses.js
+++ backend/addCourses.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 
     $.ajax({
-        url: 'https://stagingfacultypython.edwisely.com/getCourses',
+        url: 'https://stagingfacultypython.edwisely.com/getCourseDetails?subject_semester_id=' + subSemId,
         type: 'GET',
         contentType: 'application/json',
         headers: {
@@ -9,31 +9,82 @@ $(document).ready(function() {
         },
         success: function (result) {
             // alert(result.status);
+            $('#courseClass').empty();
+            if (result.status == 200) {
+
+                
+            }
+            else {
+                alert(result.message + " Please Login again");
+                window.location.href = "Loginpage.html";
+            }
+        },
+        error: function (error) {
+            alert(result.message);
+        }
+    });
+
+    let courses = [];
+
+    $.ajax({
+        url: 'https://stagingfacultypython.edwisely.com/getCourses',
+        type: 'GET',
+        contentType: 'application/json',
+        headers: {
+            'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxMTMwLCJlbWFpbCI6InByYWthc2hAZWR3aXNlbHkuY29tIiwiaW5pIjoiMTYwNjIzMjkxOCIsImV4cCI6IjE2MDc1Mjg5MTgifQ.i1TImgHIZx5cP6L7TAYrEwpBVpbsjmsF1mvqmiEolo4'
+        },
+        success: function (result) {
+            // alert(result.message);
             $('#courseList').empty();
             $('#courseSelectList').empty();
-            let div = '<ul class="list-unstyled">';
             if (result.status == 200) {
+                // alert("here");
+                let div1 = '<ul class="list-unstyled">';
+                let div = "";
                 $.each(result.data, function (key, value) {
+                    // alert(value.id);
                     courses.push({
-                        "subSemId":value.subSemId,
+                        "id":value.id,
                         "name":value.name
                     });
                     //  alert(value.name);
-                    div = div + "<li class='course' data-subSemId='"+value.subject_semester_id+"'>"+value.name+"</li>";
+                    div1 = div1 + "<li class='course' data-id='"+value.id+"'>"+value.name+"</li>";
 
+                    div = div + "<div class='col-sm-4 course'>";
+                    div = div + "<div class='card mb-3 shadow-sm'>";
                     if (value.course_image)
-                        $('#courseList').append("<div class='col-sm-4 course' data-subSemId='" + value.subject_semester_id + "'><div class='card mb-3 shadow-sm'><img class='card-img-top p-2' width='200' height='200' src='" + value.course_image + "' alt='No Image'><div class='card-body'><h5 class='card-title font-weight-bold'>" + value.name + "</h4></div></div></div>");
+                        div = div + "<img class='card-img-top py-2' width='200' height='200' src='"+value.course_image+"' alt='No Image'>";
                     else
-                        $('#courseList').append("<div class='col-sm-4 course' data-subSemId='" + value.subject_semester_id + "'><div class='card mb-3 shadow-sm'><img class='card-img-top p-2' width='200' height='200' src='../images/onlineCourses.png' alt='No Image'><div class='card-body'><h5 class='card-title font-weight-bold'>" + value.name + "</h4></div></div></div>");
+                        div = div + "<img class='card-img-top py-2' width='200' height='200' src='../images/onlineCourses.png' alt='No Image'>";
+                    // div = div + "<img class='card-img-top py-2' width='200' height='200' src='../images/onlineCourses.png' alt='No Image'>";
+                    div = div + "<div class='card-body'>";
+                    div = div + "<h5 class='card-title font-weight-bold pb-0 mb-0'>"+value.name+"</h5>";
+                    div = div + "<p class='py-0 my-0'><span class='span-heading'>Departments</span></p>";
+                    div = div + "<div class='row'>";
+
+                    $.each(value.departments, function (key, value) {
+                        div = div + "<div class='col-sm-3 mb-2'>";
+                        div = div + "<span class='span-heading span-dept'>"+value.name+"</span>";
+                        div = div + "</div>";
+                    });
+                    
+                    div = div + "</div><div class='d-flex py-0 justify-content-end'>";
+                    div = div + "<button type='button' class='btn btn-primary' data-id='" + value.id + "'>+</button>";
+                    div = div + "</div></div></div></div>";
+
+                    
                 });
-                div = div + "</ul>";
-                $('#courseSelectList').append(div);
+                div1 = div1 + "</ul>";
+                // alert(div1);
+                $('#courseList').append(div);
+
+                $('#courseSelectList').append(div1);
             }
             else
                 $('#courseList').append("<div class='col-sm-12 mb-5'><h5>No Courses Found</h5></div>");
         },
         error: function (error) {
-            alert(result.message);
+            alert(error);
         }
     });
 
