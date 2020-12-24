3fa8fc08f1e72fffeafe17d9e275f63e9712feed
Mimoh Kulkarni <mimohkulkarni17@gmail.com>
2020-12-24 13:53:29 +0530

    bug fixes

--- backend/addQues.js
+++ backend/addQues.js
@@ -195,6 +195,7 @@ $(document).ready(function () {
               editQuesJSON = $.cookie('editQues') ? JSON.parse($.cookie('editQues')) : null;
               questions.push(editQuesJSON);
               isCookie = true;
+              $('#questionBankA').attr('href','courseDetails.html?id=' + sId + '&uid=' + unit_id);
               loadList();
               $.cookie('editQues', "");
               $('#questionId').val(editQuesJSON.id);
@@ -1351,31 +1352,31 @@ $(document).ready(function () {
         if (index == 0 && (option1 || option1_img || option1_img_url)) {
           if (answer == "0") e.is_answer = "1";
           else e.is_answer = "0";
-          if (option1_img || option1_img_url) e.media = "1";
+          if (option1_img || option1_img_url) e.media = 1;
           return e;
         }
         if (index == 1 && (option2 || option2_img || option2_img_url)) {
           if (answer == "1") e.is_answer = "1";
           else e.is_answer = "0";
-          if (option2_img || option2_img_url) e.media = "1";
+          if (option2_img || option2_img_url) e.media = 1;
           return e;
         }
         if (index == 2 && (option3 || option3_img || option3_img_url)) {
           if (answer == "2") e.is_answer = "1";
           else e.is_answer = "0";
-          if (option3_img || option3_img_url) e.media = "1";
+          if (option3_img || option3_img_url) e.media = 1;
           return e;
         }
         if (index == 3 && (option4 || option4_img || option4_img_url)) {
           if (answer == "3") e.is_answer = "1";
           else e.is_answer = "0";
-          if (option4_img || option4_img_url) e.media = "1";
+          if (option4_img || option4_img_url) e.media = 1;
           return e;
         }
         if (index == 4 && (option5 || option5_img || option5_img_url)) {
           if (answer == "4") e.is_answer = "1";
           else e.is_answer = "0";
-          if (option5_img || option5_img_url) e.media = "1";
+          if (option5_img || option5_img_url) e.media = 1;
           return e;
         }
       });
